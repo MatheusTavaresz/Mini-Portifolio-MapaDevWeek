d76f6aaeaeea76555d0c84c01da3ed0580c1621f
Matheus Tavares <mathtg2@gmail.com>
2022-10-15 12:34:56 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+const abas = document.querySelectorAll(".aba")
+
+abas.forEach(aba => {
+  aba.addEventListener("click", function() {
+
+    const abaSelecionada = document.querySelector(
+      ".aba.selecionada")
+      abaSelecionada.classList.remove('selecionada')
+
+      aba.classList.add('selecionada')
+      
+      const informacaoSelecionada=document.querySelector(".informacao.selecionado")
+      informacaoSelecionada.classList.remove("selecionado")
+
+      const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+
+      const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
+      informacaoASerMostrada.classList.add("selecionado")
+
+  })
+})
